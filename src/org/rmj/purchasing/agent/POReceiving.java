2a892720c80f9ef83a9c68e8038445729af14a5a
MayNevVal <97944690+mnv121@users.noreply.github.com>
2025-03-12 11:49:59 +0800

    delete statement validation

--- src/org/rmj/purchasing/agent/POReceiving.java
+++ src/org/rmj/purchasing/agent/POReceiving.java
@@ -722,9 +722,9 @@ public class POReceiving {
                     break;
                 }
             }
-            if (lnCtr == laSubUnit.size() - 1) {
+            if (lnCtr <= laSubUnit.size() - 1) {
                 lsSQL = "DELETE FROM " + poDetail.getTable()
-                        + " WHERE nEntryNox = " + SQLUtil.toSQL(laSubUnit.get(lnCtr).getEntryNox())
+                        + " WHERE nEntryNox >= " + SQLUtil.toSQL(laSubUnit.get(lnCtr).getEntryNox())
                         + " AND sTransNox = " + SQLUtil.toSQL(laSubUnit.get(lnCtr).getTransNox());
 
                 if (!lsSQL.equals("")) {
--- src/org/rmj/purchasing/agent/PurchaseOrders.java
+++ src/org/rmj/purchasing/agent/PurchaseOrders.java
@@ -577,9 +577,9 @@ public class PurchaseOrders {
                     break;
                 }
             }
-            if (lnCtr == laSubUnit.size() - 1) {
+            if (lnCtr <= laSubUnit.size() - 1) {
                 lsSQL = "DELETE FROM " + poDetail.getTable()
-                        + " WHERE nEntryNox = " + SQLUtil.toSQL(laSubUnit.get(lnCtr).getEntryNox())
+                        + " WHERE nEntryNox >= " + SQLUtil.toSQL(laSubUnit.get(lnCtr).getEntryNox())
                         + " AND sTransNox = " + SQLUtil.toSQL(laSubUnit.get(lnCtr).getTransNox());
 
                 if (!lsSQL.equals("")) {
